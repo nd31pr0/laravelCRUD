cfbd1edebf8955f486a93631002b3d00cc8f7104
nd319R0 <ndzinfor@gmail.com>
2021-12-03 12:09:03 +0100

    initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'CRUDApp') }}</title>
+
+    </head>
+    <body class="antialiased">
+        <h1>Welcome To Laravel</h1>
+        <p>This is an application geared at Laravel CRUD functionality</p>
+    </body>
+</html>
